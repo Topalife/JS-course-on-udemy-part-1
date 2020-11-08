86e14d4a37bbc4e8c0fb2a6895e6de2d01b91bf4
Topalife <Topalife.world@github.com>
2020-11-08 17:13:28 +0100

    test
    test

---
+++ script.js
@@ -0,0 +1,91 @@
+//Values and Variables
+// are values
+/*
+let js = "amazing"
+console.log(40 + 8 + 23 - 10);
+console.log("Pavel")
+console.log(23);
+
+//are variables
+
+let firstName = "Pavel";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let pavel_maryna = "PM";
+let $function = 30;
+
+let person = "pavel";
+let PI = 3.1415;
+
+let myFirstJob = 'Web developer';
+let myCurrentJob = 'Courier';
+
+let job1 = 'programmer';
+let job2 = 'courier';
+
+console.log(myFirstJob);
+*/
+
+//Data Types
+//Primitive data types - 7
+// 1. number let age = 30;
+// 2. string let firstName = 'Pavel';
+// 3.Boolean let fullage = true;
+// 4.Undefined let children;
+// 5. Null 'empty value';
+// 6. Symbol
+// 7.Biglnt 
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 30);
+//console.log(typeof 'Pavel');
+
+//dinamic typing
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1989;
+console.log(typeof year);
+console.log(typeof null);
+*/
+
+//Basic Opetators
+
+const agePavel = 2037 - 1989;
+const ageMaryna = 2037 - 2011;
+console.log(agePavel, ageMaryna);
+console.log(agePavel * 2, ageMaryna / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Pavel';
+const lastName = 'Tyshchenko';
+console.log(firstname + ' ' + lastName);
+// ' ' - space
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--; // x = x - 2
+console.log(x);
+
+//comparison operators
+console.log(agePavel > ageMaryna);
+console.log(ageMaryna >= 18);
+
+const isFullage = ageMaryna >= 18;
+
+console.log(now - 1991 > now - 2020)
+
